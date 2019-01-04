bf5f448941b098545c4f8a69d5b507e3c77e4dcc
lifeng <beeleelee@outlook.com>
2019-01-04 11:23:20 +0800

    config publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const $common = path.join(__dirname, 'src/common')
 const $modules = path.join(__dirname, 'src/modules')
 
 module.exports = {
+  publicPath: '/',
   alias: {
     $components: path.join($common, 'components'),
     $config: path.join($common, 'config'),
